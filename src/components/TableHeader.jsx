fa8b1baacf5dba48f08ccb17e07865eaf87d6f4d
matek10000 <mateusz.dybas@microsoft.wsei.edu.pl>
2024-11-05 14:55:59 +0100

    Lab5 - #3 update (dodanie sortowania do tabeli)

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -1,23 +1,46 @@
 import React, { useState } from 'react';
 
-const TableHeader = ({ onSort }) => {
-  const [sortOrder, setSortOrder] = useState('default');
+const TableHeader = ({ onSortUser, onSortTitle, onSortComments }) => {
+  const [sortOrderUser, setSortOrderUser] = useState('default');
+  const [sortOrderTitle, setSortOrderTitle] = useState('default');
+  const [sortOrderComments, setSortOrderComments] = useState('default');
 
-  const handleSort = (order) => {
-    setSortOrder(order);
-    onSort(order);
+  const handleSortUser = (order) => {
+    setSortOrderUser(order);
+    onSortUser(order);
+  };
+
+  const handleSortTitle = (order) => {
+    setSortOrderTitle(order);
+    onSortTitle(order);
+  };
+
+  const handleSortComments = (order) => {
+    setSortOrderComments(order);
+    onSortComments(order);
   };
 
   return (
     <thead>
       <tr>
-        <th>Użytkownik</th>
-        <th>Tytuł Postu
-            <button onClick={() => handleSort('asc')}>⬆</button>
-            <button onClick={() => handleSort('desc')}>⬇</button>
-            <button onClick={() => handleSort('default')}>🔄</button>
+        <th>
+          Użytkownik
+          <button onClick={() => handleSortUser('asc')}>⬆</button>
+          <button onClick={() => handleSortUser('desc')}>⬇</button>
+          <button onClick={() => handleSortUser('default')}>🔄</button>
         </th>
-        <th>Liczba Komentarzy</th>
+        <th>
+          Tytuł Postu
+          <button onClick={() => handleSortTitle('asc')}>⬆</button>
+          <button onClick={() => handleSortTitle('desc')}>⬇</button>
+          <button onClick={() => handleSortTitle('default')}>🔄</button>
+        </th>
+        <th>
+          Liczba Komentarzy
+          <button onClick={() => handleSortComments('asc')}>⬆</button>
+          <button onClick={() => handleSortComments('desc')}>⬇</button>
+          <button onClick={() => handleSortComments('default')}>🔄</button>
+        </th>
       </tr>
     </thead>
   );
--- src/pages/Lab5.jsx
+++ src/pages/Lab5.jsx
@@ -6,7 +6,9 @@ const Lab5Page = () => {
   const [posts] = useFetch("https://jsonplaceholder.typicode.com/posts");
   const [users] = useFetch("https://jsonplaceholder.typicode.com/users");
   const [comments] = useFetch("https://jsonplaceholder.typicode.com/comments");
-  const [sortOrder, setSortOrder] = useState('default');
+  const [sortOrderUser, setSortOrderUser] = useState('default');
+  const [sortOrderTitle, setSortOrderTitle] = useState('default');
+  const [sortOrderComments, setSortOrderComments] = useState('default');
 
   if (!posts || !users || !comments) {
     return <div>Ładowanie danych...</div>;
@@ -19,25 +21,53 @@ const Lab5Page = () => {
   }));
 
   const sortedData = () => {
-    switch (sortOrder) {
-      case 'asc':
-        return [...tableData].sort((a, b) => a.post.title.localeCompare(b.post.title));
-      case 'desc':
-        return [...tableData].sort((a, b) => b.post.title.localeCompare(a.post.title));
-      default:
-        return tableData;
+    let sorted = [...tableData];
+    
+    // Sortowanie po nazwie użytkownika
+    if (sortOrderUser === 'asc') {
+      sorted.sort((a, b) => a.user.name.localeCompare(b.user.name));
+    } else if (sortOrderUser === 'desc') {
+      sorted.sort((a, b) => b.user.name.localeCompare(a.user.name));
     }
+
+    // Sortowanie po tytule postu
+    if (sortOrderTitle === 'asc') {
+      sorted.sort((a, b) => a.post.title.localeCompare(b.post.title));
+    } else if (sortOrderTitle === 'desc') {
+      sorted.sort((a, b) => b.post.title.localeCompare(a.post.title));
+    }
+
+    // Sortowanie po liczbie komentarzy
+    if (sortOrderComments === 'asc') {
+      sorted.sort((a, b) => a.comments.length - b.comments.length);
+    } else if (sortOrderComments === 'desc') {
+      sorted.sort((a, b) => b.comments.length - a.comments.length);
+    }
+
+    return sorted;
   };
 
-  const handleSort = (order) => {
-    setSortOrder(order);
+  const handleSortUser = (order) => {
+    setSortOrderUser(order);
+  };
+
+  const handleSortTitle = (order) => {
+    setSortOrderTitle(order);
+  };
+
+  const handleSortComments = (order) => {
+    setSortOrderComments(order);
   };
 
   return (
     <div>
-      <h1>Lab 5</h1>
+      <h1>Laboratorium 5</h1>
       <table>
-        <TableHeader onSort={handleSort} />
+        <TableHeader 
+          onSortUser={handleSortUser} 
+          onSortTitle={handleSortTitle} 
+          onSortComments={handleSortComments} 
+        />
         <tbody>
           {sortedData().map((data, index) => (
             <tr key={index}>
